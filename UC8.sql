579994c2c4b3c71f4eeb94aa07ee26a2db488208
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-11-02 22:57:51 +0530

    [Kartikeya] UC added to add multiple department values for single person

--- UC8.sql
+++ UC8.sql
@@ -18,3 +18,10 @@ alter table employee_payroll add IncomeTax decimal;
 alter table employee_payroll add NetPay decimal;
 select * from employee_payroll;
 
+/*UC 10:
+Make employee part of multiple departments*/
+use payroll_service;
+/*Adding additional data to table*/
+update employee_payroll set PhnNo=7206183244, Address='Techman City', Department='Sales', BasicPay=72000, Deductions=5000, TaxablePay=60000, NetPay=44000 where EmpName='Kartikeya';
+/*Adding multiple department values for a single person*/
+insert into employee_payroll values('Kartikeya',72000,'2020-09-18','M',7206183244,'Marketing','Techman City',5000,60000,2000,44000);
