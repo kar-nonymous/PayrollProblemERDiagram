cb466c955bc210386ab9b676f29d6bfc6756d73b
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-11-02 14:33:32 +0530

    [Kartikeya] UC added to rename and add more columns into the table

--- UC8.sql
+++ UC8.sql
@@ -5,3 +5,16 @@ use payroll_service;
 alter table employee_payroll add PhnNo varchar(250);
 alter table employee_payroll add Address varchar(250) default 'Homeless';
 alter table employee_payroll add Department varchar(150) not null default 'Null';
+
+/*UC 9:
+Added columns of basic pay, deductions, taxable pay, income tax and net pay*/
+use payroll_service;
+/*Renaming the salary column to basicPay*/
+EXEC sp_RENAME 'employee_payroll.Salary' , 'BasicPay', 'COLUMN';
+/*Altering the table*/
+alter table employee_payroll add Deductions decimal;
+alter table employee_payroll add TaxablePay decimal;
+alter table employee_payroll add IncomeTax decimal;
+alter table employee_payroll add NetPay decimal;
+select * from employee_payroll;
+
