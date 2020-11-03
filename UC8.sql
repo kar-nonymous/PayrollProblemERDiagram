ac41d061b17b1afb408347b2828de2ec72b8fb61
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-11-03 18:22:57 +0530

    [Kartikeya] UC added to implement e r diagram

--- UC8.sql
+++ UC8.sql
@@ -25,3 +25,51 @@ use payroll_service;
 update employee_payroll set PhnNo=7206183244, Address='Techman City', Department='Sales', BasicPay=72000, Deductions=5000, TaxablePay=60000, NetPay=44000 where EmpName='Kartikeya';
 /*Adding multiple department values for a single person*/
 insert into employee_payroll values('Kartikeya',72000,'2020-09-18','M',7206183244,'Marketing','Techman City',5000,60000,2000,44000);
+
+/*UC 11:
+ER diagram*/
+use payroll_service;
+
+/*Company table and adding data to it*/
+create table company(CompanyId int not null primary key, CompanyName varchar(150) not null);
+insert into company values(500,'Flipkart');
+insert into company values(600,'Myntra');
+select * from company;
+
+/*Employee table and adding data to it*/
+create table employee 
+(EmpId int not null identity(1,1) primary key,
+EmpName varchar(150) not null,
+Gender char(1) not null,
+PhoneNo bigint not null,
+Address varchar(500) not null,
+StartDate date not null,
+CompanyId int not null foreign key references company(CompanyId));
+insert into employee values ('Kartikeya','M',7206183244,'Agra','2020-09-18',500);
+insert into employee values ('Johansson','F',9457127771,'New York','1996-10-24',500);
+insert into employee values ('Reynolds','M',9410884202,'Canada','2003-12-26',500);
+insert into employee values ('Freeman','M',935054985,'Tennessee','1964-08-25',600);
+insert into employee values ('Roberts','F',8521479630,'Georgia','1985-10-19',600);
+
+/*Payroll table and adding data to it*/ 
+create table payroll
+(EmpId int not null primary key references employee(EmpId), BasicPay decimal,Deductions decimal,TaxablePay decimal,IncomeTax decimal,NetPay decimal);
+insert into payroll values (1,72000,3000,69000,1200,85200);
+insert into payroll values (2,160000,15000,145000,2400,74100);
+insert into payroll values (3,3200000,20000,3180000,3600,96300);
+insert into payroll values (4,450000,50000,400000,4800,85400);
+insert into payroll values (5,5600000,60000,5540000,6000,85600);
+
+/*Department table and adding data to it*/
+create table department (DeptId int not null primary key, DeptName varchar(100) not null);
+insert into department values (5,'Sales');
+insert into department values (10,'Marketing');
+
+/*EmpDepartment table and adding data to it*/
+create table empDepartment (EmpId int not null foreign key references employee(EmpId), DeptId int not null foreign key references department(DeptId));
+insert into empDepartment values (1,5);
+insert into empDepartment values (2,5);
+insert into empDepartment values (2,10);
+insert into empDepartment values (3,5);
+insert into empDepartment values (4,10);
+insert into empDepartment values (5,10);
